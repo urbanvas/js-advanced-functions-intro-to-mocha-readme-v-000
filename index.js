8c7038e0f671ea909b42b61481e4371dc1636d5f
Neil Seejoor <nvseejoor@gmail.com>
2019-01-24 23:10:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth() {
+  return true
+}
+function sayHey() {
+  return 'Hey!'
+}
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(f) {
+  return `I love ${f}`
+}
+function shouting(s) {
+  return s.toUpperCase()
+}
+function roundDown(n) {
+  return Math.floor(n)
+}
